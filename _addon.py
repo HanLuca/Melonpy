16d3f4ac4e69aa75d42c5fcaccff37b4e8381e2b
HanLuca <108270463+HanLuca@users.noreply.github.com>
2024-09-10 02:06:51 +0000

    🚀 DEPLOY IN REPLIT

--- _addon.py
+++ _addon.py
@@ -3,7 +3,11 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 options = webdriver.ChromeOptions()
+
 options.add_argument("headless")
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
 driver = webdriver.Chrome(options=options)
 
 class MelonAddon:
