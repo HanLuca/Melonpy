97393e02adf005932bd01d3feb900b6d101a3a91
HanLuca <hanluca2022@gmail.com>
2024-09-12 10:40:29 +0900

    ✨Session Reset Task

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_session import Session
+from datetime import timedelta
 import random
 
 from main_page import main_page
@@ -9,7 +10,8 @@ from config import token
 
 app = Flask(__name__)
 
-app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_PERMANENT'] = True
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(seconds=3)
 app.config["SESSION_TYPE"] = "filesystem"
 
 Session(app)
